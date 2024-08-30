dde1d0aa713bd32441f4bdb909291820a90cecdd
DavidYeLuo <davidayeluo@gmail.com>
2024-08-30 10:38:21 -0700

    Refactor level implementation.

--- Assets/Scripts/Levels/FirstLevel.cs
+++ Assets/Scripts/Levels/FirstLevel.cs
@@ -4,73 +4,32 @@ using System.Collections;
 
 namespace Level {
 public class FirstLevel : MonoBehaviour {
-  [Header("Camera/Scenematics")]
   [SerializeField]
-  private float cameraOffset;
-  [SerializeField]
-  private float transitionTime;
-
-  [Header("Dependencies")]
-  [SerializeField]
-  private Player.Player player;
+  private Level levelHelper;
   [SerializeField]
   private UtensilBox utensilBox;
 
-  private Camera cam;
-  private GameManager gameManager;
-
-  // TODO: Instead of the level handling the animation cutscene, let's offload
-  // it to another gameobject
-  private IEnumerator Start() {
-    gameManager = GameManager.Instance();
-    cam = Camera.main;
-
-    Vector3 temp =
-        cam.transform
-            .position; // For returning the camera where it needs to be in case
-
-    cam.transform.position -= cameraOffset * cam.transform.forward;
-
-    // Start Cutscene
-    // BUG: when disabling playerscript, it prevents the user to pause in mid
-    // cutscene
-    player.enabled = false; // Disables input while in cutscene
-    const float INTERVAL = 60;
-    float wait = transitionTime / INTERVAL;
-    float step = cameraOffset / INTERVAL;
-    // BUG: Waiting for a second is too long
-    // We should find a different way to animate because the only time that is
-    // smooth is when wait = 0
-    // TODO: Research for custom lerp implementation
-    for (int i = 0; i < INTERVAL; i++) {
-      cam.transform.position += step * cam.transform.forward;
-      yield return new WaitForSeconds(wait);
-    } // End of Cutscene
-    cam.transform.position = temp; // Return the camera where it original was in
-                                   // case it gets lost after the animation.
-                                   // Might not be needed.
-    player.enabled = true;         // Enables input while in cutscene
-
-    yield return null;
+  private void Awake() { levelHelper.Init(); }
+  private void Start() {
+    StartCoroutine(levelHelper.ZoomInToPlayerTransition(
+        Level.DEFAULT_CAMERA_OFFSET, Level.DEFAULT_TRANSITION_TIME));
   }
 
   private void WinWhenUtensilBoxIsSorted(bool isSorted) {
     if (!isSorted)
       return;
     Debug.Log("[Event invoke, FirstLevel] UtensilBox sorted! You Win!");
-    gameManager.WinGame();
+    levelHelper.gameManager.WinGame();
   }
-
-  private void Pause() { gameManager.TogglePause(); }
 
   private void OnEnable() {
     utensilBox.isUtensilBoxSortedHandler += WinWhenUtensilBoxIsSorted;
-    player.pauseHandler += Pause;
+    levelHelper.player.pauseHandler += levelHelper.gameManager.TogglePause;
   }
 
   private void OnDisable() {
     utensilBox.isUtensilBoxSortedHandler -= WinWhenUtensilBoxIsSorted;
-    player.pauseHandler -= Pause;
+    levelHelper.player.pauseHandler -= levelHelper.gameManager.TogglePause;
   }
 }
 }
--- Assets/Scripts/Levels/SecondLevel.cs
+++ Assets/Scripts/Levels/SecondLevel.cs
@@ -6,78 +6,35 @@ using Interactable;
 
 namespace Level {
 public class SecondLevel : MonoBehaviour {
-  [Header("Camera/Scenematics")]
   [SerializeField]
-  private float cameraOffset;
-  [SerializeField]
-  private float transitionTime;
-
-  [Header("Dependencies")]
-  [SerializeField]
-  private Player.Player player;
+  private Level levelHelper;
   [SerializeField]
   private SpeedRack speedrack;
 
-  private Camera cam;
-  private GameManager gameManager;
-
-  // TODO: Instead of the level handling the animation cutscene, let's offload
-  // it to another gameobject
-  private IEnumerator Start() {
-    gameManager = GameManager.Instance();
-    cam = Camera.main;
-
-    Vector3 temp =
-        cam.transform
-            .position; // For returning the camera where it needs to be in case
-
-    cam.transform.position -= cameraOffset * cam.transform.forward;
-
-    // Start Cutscene
-    // BUG: when disabling playerscript, it prevents the user to pause in mid
-    // cutscene
-    player.enabled = false; // Disables input while in cutscene
-    const float INTERVAL = 60;
-    float wait = transitionTime / INTERVAL;
-    float step = cameraOffset / INTERVAL;
-    // BUG: Waiting for a second is too long
-    // We should find a different way to animate because the only time that is
-    // smooth is when wait = 0
-    // TODO: Research for custom lerp implementation
-    for (int i = 0; i < INTERVAL; i++) {
-      cam.transform.position += step * cam.transform.forward;
-      yield return new WaitForSeconds(wait);
-    } // End of Cutscene
-    cam.transform.position = temp; // Return the camera where it original was in
-                                   // case it gets lost after the animation.
-                                   // Might not be needed.
-    player.enabled = true;         // Enables input while in cutscene
-
-    yield return null;
+  private void Awake() { levelHelper.Init(); }
+  private void Start() {
+    StartCoroutine(levelHelper.ZoomInToPlayerTransition(
+        Level.DEFAULT_CAMERA_OFFSET, Level.DEFAULT_TRANSITION_TIME));
   }
-
-  private void Pause() { gameManager.TogglePause(); }
-
-  private void WinWhenSpeedRackIsFinished(ItemHolderState state) {
+  private void WinWhenSpeedRackHasMuffinTrays(ItemHolderState state) {
     if (state != ItemHolderState.FILLED)
       return;
 
     var food = speedrack.PeekTopItem();
     if (food != null && food is FoodTray &&
         food.GetState() == ItemHolderState.FILLED)
-      gameManager.WinGame();
+      levelHelper.gameManager.WinGame();
     else
       Debug.Log("TODO: Empty foodtray not implemented.");
   }
-
   private void OnEnable() {
-    player.pauseHandler += Pause;
-    speedrack.foodHolderHandler += WinWhenSpeedRackIsFinished;
+    speedrack.foodHolderHandler += WinWhenSpeedRackHasMuffinTrays;
+    levelHelper.player.pauseHandler += levelHelper.gameManager.TogglePause;
   }
 
   private void OnDisable() {
-    player.pauseHandler -= Pause;
-    speedrack.foodHolderHandler -= WinWhenSpeedRackIsFinished;
+    speedrack.foodHolderHandler -= WinWhenSpeedRackHasMuffinTrays;
+    levelHelper.player.pauseHandler -= levelHelper.gameManager.TogglePause;
   }
 }
 }
