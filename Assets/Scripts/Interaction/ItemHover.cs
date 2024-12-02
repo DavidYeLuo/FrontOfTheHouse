7aa26e6edcec5cae28f17268e45146dc8cc7626e
DavidYeLuo <davidayeluo@gmail.com>
2024-12-02 09:53:28 -0800

    Add warning when using highlight material when the material is null.

--- Assets/Scripts/Interaction/ItemHover.cs
+++ Assets/Scripts/Interaction/ItemHover.cs
@@ -21,14 +21,38 @@ public class ItemHover : MonoBehaviour, IHover {
   [SerializeField]
   private UnityEvent _OnHoverExit;
 
+  public Material highlightMaterial;
+  private List<Material> highlightMaterials;
+  private List<Material> originalMaterial;
+
   [Header("Dependency")]
   public GameObject Visual;
+  [SerializeField]
+  private List<Renderer> targetRendererList;
 
   private void Start() {
     if (useEditorScale)
       return;
     HoverScale = new Vector3(1.1f, 1.1f, 1.1f);
     OriginalScale = transform.localScale;
+
+    // TODO: generate a highlight material
+    originalMaterial = new List<Material>();
+    highlightMaterials = new List<Material>();
+    foreach (var targetRenderer in targetRendererList) {
+      Color previousColor = targetRenderer.material.color;
+      originalMaterial.Add(targetRenderer.material);
+      targetRenderer.material = highlightMaterial;
+      targetRenderer.material.SetColor("_BaseColor", previousColor);
+      // targetRenderer.material.SetFloat("_Thickness", 3.0f);
+      highlightMaterials.Add(targetRenderer.material);
+    }
+    for (int i = 0; i < targetRendererList.Count; i++) {
+      targetRendererList[i].material = originalMaterial[i];
+    }
+    if (highlightMaterial == null)
+      Debug.LogWarning(
+          $"Id: {this.gameObject.name}, Highlight material in ItemHover.cs is null. Calling SetHighlight Material is going to be crash.");
   }
 
   public void OnHoverEnter() { _OnHoverEnter?.Invoke(); }
@@ -39,5 +63,15 @@ public class ItemHover : MonoBehaviour, IHover {
   public void SetToHoverScale() { Visual.transform.localScale = HoverScale; }
 
   public void ResetScale() { Visual.transform.localScale = OriginalScale; }
+  public void SetToHighlightMaterial() {
+    for (int i = 0; i < targetRendererList.Count; i++) {
+      targetRendererList[i].material = highlightMaterials[i];
+    }
+  }
+  public void SetToOriginalMaterial() {
+    for (int i = 0; i < targetRendererList.Count; i++) {
+      targetRendererList[i].material = originalMaterial[i];
+    }
+  }
 }
 }
